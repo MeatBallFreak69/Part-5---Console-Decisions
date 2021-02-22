48127f4e5b0e8a029ff875d9c94ce63bab161c22
Clark Miller <glitchclick@gmail.com>
2021-02-22 15:18:34 -0500

    Used switches

--- Part 5 - Console Decisions/Program.cs
+++ Part 5 - Console Decisions/Program.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Part_5___Console_Decisions
 {
     class Program
+
     {
+
         static void Main(string[] args)
         {
             int intAge;
@@ -32,6 +35,33 @@ namespace Part_5___Console_Decisions
             }
             else if(intAge <= 0) {
                 Console.WriteLine("Error: Your age can't be below 0");
+            }
+            int windspeed;
+            Console.WriteLine("Input your current windspeed category:)");
+            int.TryParse(Console.ReadLine(), out windspeed);
+            
+            switch (windspeed)
+            {
+                case 1:
+                    Console.WriteLine("Category 1: 74-95 mph or 64-82 kt or 119-153 km/hr");
+                    break;
+                case 2:
+                    Console.WriteLine("Category 2: 96-110 mph or 83-95 kt or 154-177 km/hr");
+                    break;
+                case 3:
+                    Console.WriteLine("Category 3: 111-130 mph or 96-113 kt or 178-209 km/hr");
+                    break;
+                case 4:
+                    Console.WriteLine("Category 4: 131-155 mph or 114-135 kt or 210-249 km/hr");
+                    break;
+                case 5:
+                    Console.WriteLine("Category 5: greater than 155 mph or 135 kt or 249 km/hr");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Category");
+                    break;
+         
+                    
             }
             Console.ReadLine();
         }
