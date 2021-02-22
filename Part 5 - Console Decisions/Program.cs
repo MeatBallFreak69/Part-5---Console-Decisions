8d49595d698bd9c08935e270a39f990de30cb615
Clark Miller <glitchclick@gmail.com>
2021-02-22 14:18:41 -0500

    Add project files.

---
+++ Part 5 - Console Decisions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_5___Console_Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intAge;
+            string strInput;
+            Console.WriteLine("State your age");
+            strInput = Console.ReadLine();
+            int.TryParse(strInput, out intAge);
+            if (intAge >= 18 ) {
+                Console.WriteLine("adult");
+            }
+            else if(intAge <= 5) {
+                Console.WriteLine("Toddler");
+            }
+            else if(intAge <= 10 & intAge > 5) {
+                Console.WriteLine("Child");
+            }
+            else if(intAge <= 12 & intAge > 10) {
+                Console.WriteLine("PreTeen");
+            }
+            else if(intAge < 12 & intAge > 18) {
+                Console.WriteLine("Teen");
+            }
+            Console.ReadLine();
+        }
+    }
+}
